a9f12113e1dfa75d2e8800461b2962b2ee108973
Quynh Nguyen <quynhnh@gmail.com>
2020-12-20 16:31:12 +0000

    Download utility using paused stream

--- download.js
+++ download.js
@@ -24,7 +24,7 @@ const downloadFlowing = function (url, saveAsFile) {
     response.on('data', (chunk) => {
       i++
       writer.write(chunk)
-      console.log(`chunk ${i}`)
+      console.log(`chunk ${i}\tsize ${chunk.length}`)
     }).on('end', () => {
       writer.close()
       console.log('Download done')
@@ -37,6 +37,31 @@ const downloadFlowing = function (url, saveAsFile) {
   })
 }
 
-const url = 'https://www.freemaptools.com/download/outcode-postcodes/postcode-outcodes.csv'
-const fileName = 'test.txt'
-downloadFlowing(url, fileName)
+const downloadPaused = function (url, saveAsFile) {
+  const writer = fs.createWriteStream(saveAsFile)
+  const request = http.get(url, (response) => {
+    let i = 0
+    let chunk = ''
+    response.on('readable', () => {
+      i++
+      while ((chunk = response.read()) !== null) {
+        writer.write(chunk)
+        console.log(`chunk ${i}\tsize ${chunk.length}`)
+      }
+    }).on('end', () => {
+      writer.close()
+      console.log('Download done')
+    })
+
+    console.log('Turn the tap on!')
+  }).on('error', (err) => {
+    fs.unlink(saveAsFile)
+    console.error(err)
+  })
+}
+
+// const url = 'https://www.freemaptools.com/download/outcode-postcodes/postcode-outcodes.csv'
+// const fileName = 'test.txt'
+const url = 'https://www.freemaptools.com/download/full-postcodes/ukpostcodes.zip'
+const fileName = 'test.zip'
+downloadPaused(url, fileName)
