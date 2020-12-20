e7003c79c304db791d959422b6549db2e57b8284
Quynh Nguyen <quynh.nguyen@simplybusiness.co.uk>
2020-12-20 16:19:21 +0000

    Download utility

---
+++ download.js
@@ -0,0 +1,42 @@
+const http = require('https')
+const fs = require('fs')
+
+const downloadPipe = function (url, saveAsFile, callback) {
+  const writer = fs.createWriteStream(saveAsFile)
+  const request = http.get(url, (response) => {
+    response.pipe(writer)
+    writer.on('finish', () => {
+      writer.close()
+      console.log('Download done')
+    })
+    console.log('Turn the tap on!')
+  }).on('error', (err) => {
+    fs.unlink(saveAsFile)
+    if (callback) callback(err.message)
+  })
+}
+
+const downloadFlowing = function (url, saveAsFile) {
+  const writer = fs.createWriteStream(saveAsFile)
+  const request = http.get(url, (response) => {
+    let i = 0
+
+    response.on('data', (chunk) => {
+      i++
+      writer.write(chunk)
+      console.log(`chunk ${i}`)
+    }).on('end', () => {
+      writer.close()
+      console.log('Download done')
+    })
+
+    console.log('Turn the tap on!')
+  }).on('error', (err) => {
+    fs.unlink(saveAsFile)
+    console.error(err)
+  })
+}
+
+const url = 'https://www.freemaptools.com/download/outcode-postcodes/postcode-outcodes.csv'
+const fileName = 'test.txt'
+downloadFlowing(url, fileName)
